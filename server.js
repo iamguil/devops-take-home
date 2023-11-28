739541736543ba6646062e7763766eefe441c3e6
iamguil <guiller.cabatuan@gmail.com>
2023-11-29 07:37:38 +0800

    test another static web page and edit server.js file to handle file from static web pages

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.APPLICATION_PORT || 8080;
 
+// Define your routes here
 app.get('/', function (req, res) {
   res.status(200);
   res.sendFile('index.html', { root: __dirname });
 });
+
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
+// Start the server
 const server = app.listen(port, () => {
   console.log(`App listening on ${port}`);
 });
 
-module.exports = { app, server }; 
+module.exports = { app, server };
